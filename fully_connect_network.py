0e24a714cb7f92b973ac663c34c151039dbc219d
marvel <1722247182@qq.com>
2026-01-04 11:13:18 +0800

    improve the model argument

--- fully_connect_network.py
+++ fully_connect_network.py
@@ -7,11 +7,11 @@ class FullConnect(nn.Module):
     def __init__(self):
         super(FullConnect, self).__init__()
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.1)
-        self.linear1 = nn.Linear(5, 32)  # 增加隐藏层神经元数量
-        self.linear2 = nn.Linear(32, 64)
-        self.linear3 = nn.Linear(64, 32)
-        self.linear4 = nn.Linear(32, 2)
+        self.dropout = nn.Dropout(0.2)
+        self.linear1 = nn.Linear(5, 25)  # 增加隐藏层神经元数量
+        self.linear2 = nn.Linear(25, 50)
+        self.linear3 = nn.Linear(50, 25)
+        self.linear4 = nn.Linear(25, 2)
         # 不需要softmax，CrossEntropyLoss会自动处理
 
     def forward(self, x):
